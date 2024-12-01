7b987c0f551473d3730272ce8e3d3fd13da2f402
Alexander Yoshida <alexander.yoshida@gmail.com>
2024-12-01 16:31:56 -0500

    init setup

---
+++ day1a.cpp
@@ -0,0 +1,11 @@
+#include "./setup.h"
+
+int main() {
+    // env setup
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    // cout.tie(0); // comment out for baekjoon
+    cout << fixed << setprecision(12);
+
+    cout << "Hello World" << endl;
+}
---
+++ run.ps1
@@ -0,0 +1,25 @@
+param(
+    [string]$day,
+    [bool]$recompile=$true
+)
+
+$path = -join("./day", $day, ".cpp")
+if (-not(Test-Path -Path './tmp')) {
+    New-Item -Path './tmp' -ItemType Directory | Out-Null
+}
+if ($recompile) {
+    if (Test-Path -Path './tmp/a.exe') {
+        Remove-Item ./tmp/a.exe
+    }
+    if (Test-Path -Path './tmp/main.txt') {
+        Remove-Item ./tmp/main.txt
+    }
+    g++ -std=c++17 -W -Wall -O3 -o './tmp/a.exe' -g $path ./setup.h
+}
+if (Test-Path -Path './output.txt') {
+    Remove-Item ./output.txt
+}
+if (Test-Path -Path './tmp/a.exe') {
+    Get-Content ./input.txt | ./tmp/a.exe | Tee-Object ./output.txt
+    Copy-Item $path ./tmp/main.txt
+}
