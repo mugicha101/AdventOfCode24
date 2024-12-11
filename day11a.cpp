1e6dd86a3db07f1523a9504709d268ce8254464d
Alexander Yoshida <alexander.yoshida@gmail.com>
2024-12-11 10:36:45 -0500

    day 11

---
+++ day11a.cpp
@@ -0,0 +1,33 @@
+#include "./setup.h"
+
+ll stones(ll val, int procs) {
+  if (procs == 0) return 1;
+
+  static map<pair<ll,int>,ll> memo;
+  pair<ll,int> key = make_pair(val, procs);
+  auto it = memo.find(key);
+  if (it != memo.end()) {
+    return it->second;
+  }
+
+  if (val == 0) {
+    return memo[key] = stones(1, procs - 1);
+  }
+  string digs = to_string(val);
+  int n = digs.size();
+  if (n & 1) {
+    return memo[key] = stones(val * 2024, procs - 1);
+  }
+
+  int h = n >> 1;
+  return memo[key] = stones(stoi(digs.substr(0, h)), procs - 1) + stones(stoi(digs.substr(h, h)), procs - 1);
+}
+
+int main() {
+  int s;
+  ll sum = 0;
+  while (cin >> s) {
+    sum += stones(s, 25);
+  }
+  cout << sum << endl;
+}
--- run.ps1
+++ run.ps1
@@ -25,12 +25,14 @@ if ($recompile) {
     $filtered = $merged | Where-Object { $_ -notmatch '#include "\./setup\.h"' }
     $filtered | Out-File -Encoding ASCII ./tmp/a.cpp
     g++ -std=c++23 -W -Wall -O3 -o './tmp/a.exe' './tmp/a.cpp'
-    Write-Host compiled
 }
 if (Test-Path -Path './output.txt') {
     Remove-Item ./output.txt
 }
 if (Test-Path -Path './tmp/a.exe') {
+    Write-Host "compiled" -ForegroundColor Green
     Get-Content ./input.txt | ./tmp/a.exe | Tee-Object ./output.txt
     Copy-Item $path ./tmp/main.txt
+} else {
+    Write-Host "failed to compile" -ForegroundColor Red
 }
